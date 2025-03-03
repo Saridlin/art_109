6e29609f8481fe4a58e4b0e4b3e1953a69a53d23
Daria <45639752+Saridlin@users.noreply.github.com>
2025-03-03 03:18:51 -0800

    css essentials ch 6-7

--- tutorials/05-dom-javascript/js/main.js
+++ tutorials/05-dom-javascript/js/main.js
@@ -5,6 +5,7 @@ let pageTitle = document.querySelector("#page-title");
 let header = document.querySelector("header");
 let body = document.querySelector("body");
 
+/*
 let img0 = document.querySelector("#img0");
 let img1 = document.querySelector("#img1");
 let img2 = document.querySelector("#img2");
@@ -15,45 +16,31 @@ let img6 = document.querySelector("#img6");
 let img7 = document.querySelector("#img7");
 let img8 = document.querySelector("#img8");
 
-
-
-
 img0.addEventListener('click', () => {
     img3.style.visibility = "visible";
 });
-
-
 img3.addEventListener('click', () => {
     img7.style.visibility = "visible";
 });
-
-
 img7.addEventListener('click', () => {
     img2.style.visibility = "visible";
 });
-
 img2.addEventListener('click', () => {
     img8.style.visibility = "visible";
 });
-
 img8.addEventListener('click', () => {
     img1.style.visibility = "visible";
 });
-
 img1.addEventListener('click', () => {
     img4.style.visibility = "visible";
 });
-
 img4.addEventListener('click', () => {
     img6.style.visibility = "visible";
 });
-
 img6.addEventListener('click', () => {
     img5.style.visibility = "visible";
 });
-
-
-
+*/
 
 //for loop to add click event listener to all images
 //disappear on click
